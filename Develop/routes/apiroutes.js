a84718c873112b52448e8ebff7fa13ff878583f9
Rajahni fields <rajahnifields@gmail.com>
2021-01-29 23:53:29 -0500

    yay is pretty

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,6 +1,8 @@
 const fs =require("fs")
 var path = require("path");
 var dbWrite = require("../db/db.json") 
+//unique id
+const { v4: uuidv4 } = require('uuid');
 
 module.exports =(app)=>{
 
@@ -10,14 +12,22 @@ module.exports =(app)=>{
       });
       // grab the data that was typed
       app.post("/api/notes", function(req, res) {
+        req.body.id = uuidv4();
+        // define the uuid (unique)
         dbWrite.push(req.body)
 
         fs.writeFile(path.join(__dirname,"../db/db.json"),JSON.stringify) 
-
-        
         res.json (dbWrite)
         
       });
+      // add error if something happens
+      (dbWrite),function(err){
+
+          if(err) throw err
+            // if no err happens write the note
+          res.json(dbWrite)
+
+      }
 
 }
-console.log(dbWrite)
+// console.log(dbWrite)
